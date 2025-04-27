e5dc668c65c44a1efd27d1939034df1ff92c4f47
Ankit Jha <aajjha9@gmail.com>
2025-04-27 14:41:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+
+const API_KEY = process.env.API_KEY;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/new-message", (req, res) => {
+  const { message } = req.body;
+
+  axios
+    .post(`https://api.telegram.org/bot${API_KEY}/sendMessage`, {
+      chat_id: message.chat.id,
+      text: "Hello there i am using telegram!",
+    })
+    .then((e) => {
+      console.log(e);
+    })
+    .catch((e) => {
+      console.error("error occured");
+    });
+
+  res.json({
+    status: "200",
+    API_KEY,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Bot listening on port 3000");
+});
