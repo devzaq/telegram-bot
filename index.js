64ae8f54b271b0f6030e75d260dc73ebf8d69492
Ankit Jha <aajjha9@gmail.com>
2025-04-27 14:49:36 +0530

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/new-message", (req, res) => {
+app.post("/", (req, res) => {
   const { message } = req.body;
 
   axios
