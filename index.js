09be6d5450b4202b68e348fd8a4387d5b617eb65
Ankit Jha <aajjha9@gmail.com>
2025-04-27 15:14:59 +0530

    commit

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ app.post("/", (req, res) => {
   const { message } = req.body;
 
   if (!message || message.text.toLowerCase().indexOf("mango") < 0)
-    return res.end();
-  axios
-    .post(`https://api.telegram.org/bot${API_KEY}/sendMessage`, {
-      chat_id: message.chat.id,
-      text: "Lassi🥂",
-      //   text: "lassi " + message.text,
-    })
-    .then((e) => {
-      console.log(e);
-    })
-    .catch((e) => {
-      console.error("error occured");
-    });
+    return res.json({ status: "fail" });
+  //   axios
+  //     .post(`https://api.telegram.org/bot${API_KEY}/sendMessage`, {
+  //       chat_id: message.chat.id,
+  //       text: "Lassi🥂",
+  //       //   text: "lassi " + message.text,
+  //     })
+  //     .then((e) => {
+  //       console.log(e);
+  //     })
+  //     .catch((e) => {
+  //       console.error("error occured", e);
+  //     });
 
   res.json({
     status: "200",
